17b0de1587d0cb227b1e7b4dc9fccf93283018b1
xalswox <qoalswo123@icloud.com>
2023-06-14 10:48:54 +0900

    Feat: ScoreManager 최고 점수 저장 기능 추가
    PlayerPrefs 데이터베이스 기능 사용

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -10,8 +10,12 @@ using UnityEngine.UI;
 //점수가 갱신될때 만약 최고점수보다 크다면 최고 점수를 갱신된 점수로 바꾸고 저장하고 싶다.
 public class ScoreManager : MonoBehaviour
 {
+    // 상수 - 읽기 전용
+    readonly string highScoreKey = "HIGH_SCORE";
     public TextMeshProUGUI textScore;
+    public TextMeshProUGUI textHighScore;
     public static ScoreManager instance;
+    int highScore;
     int score;
 
     // 싱글톤 패턴 생성
@@ -22,6 +26,9 @@ public class ScoreManager : MonoBehaviour
     void Start()
     {
         SCORE = 0;
+
+        //태어날 때 최고점수를 읽어오고싶다. UI로도 표현하고 싶다.
+        HIGH_SCORE = PlayerPrefs.GetInt(highScoreKey, 0);
     }
 
     // 호출하는 입장 프로퍼티
@@ -31,12 +38,33 @@ public class ScoreManager : MonoBehaviour
         {
             score = value;
             textScore.text = "Score : " + score;
-            // 만약 Score가 HighScore보다 크다면 저장하고 싶다.
+            // 만약 Score가 HighScore보다 크다면
+            if (score > HIGH_SCORE)
+            {
+                //highScore에 scroe를 대입하고 싶다.
+                HIGH_SCORE = score;
+                // 저장하고 싶다. 데이터 베이스를 쓸 수 있다.
+                PlayerPrefs.SetInt(highScoreKey, HIGH_SCORE);
+            }
         }
 
-        get 
+        get
         {
-            return score; 
+            return score;
+        }
+    }
+
+    public int HIGH_SCORE
+    {
+        set
+        {
+            highScore = value;
+            textHighScore.text = "HighScore: " + highScore;
+        }
+
+        get
+        {
+            return highScore;
         }
     }
 }
